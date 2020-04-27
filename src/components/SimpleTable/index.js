fbbad518c97dc821ccd1c6de10011089e4a7e353
oguhpereira <guhpereira@outlook.com.br>
2020-04-27 11:23:49 -0300

    [FIX] Simple Table Component
    *  Adding tr thead
    * Adding condition renders only when there is content
    * documenting fuctions

--- src/components/SimpleTable/index.js
+++ src/components/SimpleTable/index.js
@@ -5,6 +5,12 @@
 import React from 'react';
 import './simple-table.css';
 
+/**
+ * @function TableRow
+ * @param {Array} data
+ * @param {Array} keys
+ * @returns {JSX}
+ */
 function TableRow({ data, keys }) {
 	return (
 		<tr>
@@ -17,24 +23,37 @@ function TableRow({ data, keys }) {
 	);
 }
 
-export function SimpleTable({ title, head, content, keys }) {
-	return (
-		<div className="table-content">
-			{title && <h2 className="subtitle">{title}</h2>}
-			<table className="simple-table">
-				<thead className="thead">
-					{head.map((nameTH, index) => (
-						<th className="th" key={index}>
-							{nameTH}
-						</th>
-					))}
-				</thead>
-				<tbody className="tbody">
-					{content.map((tr, index) => {
-						return <TableRow key={index} data={tr} keys={keys} />;
-					})}
-				</tbody>
-			</table>
-		</div>
-	);
+/**
+ * @function SimpleTable
+ * @param {String} title
+ * @param {Array} head
+ * @param {Array} content
+ * @param {Array} keys
+ * @returns {JSX}
+ */
+export function SimpleTable({ title, head, content = [], keys }) {
+	if (content.length > 0) {
+		return (
+			<div className="table-content">
+				{title && <h2 className="subtitle">{title}</h2>}
+				<table className="simple-table">
+					<thead className="thead">
+						<tr>
+							{head.map((nameTH, index) => (
+								<th className="th" key={index}>
+									{nameTH}
+								</th>
+							))}
+						</tr>
+					</thead>
+					<tbody className="tbody">
+						{content.map((tr, index) => {
+							return <TableRow key={index} data={tr} keys={keys} />;
+						})}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+	return <div />;
 }
