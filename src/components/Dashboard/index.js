3c27bbd2403408f4f6851592336d3ca1ab713bda
oguhpereira <guhpereira@outlook.com.br>
2020-04-29 18:13:05 -0300

    [IMPROVEMENT] Wrap Card

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -15,7 +15,6 @@ import {
 	Pie,
 	Sector,
 	PieChart,
-	Legend,
 } from 'recharts';
 
 import lang from '../../languages';
@@ -25,8 +24,8 @@ import Card from '../Card';
 function CustomizedAxisTick({ x, y, payload }) {
 	return (
 		<g transform={`translate(${x},${y})`}>
-			<text x={0} y={0} dy={16} textAnchor="end" fill="#666">
-				{`R$ ${formatMoney(payload.value)}`}
+			<text x={-3} y={-10} dy={16} textAnchor="end" fill="#666">
+				{`R$${formatMoney(payload.value)}`}
 			</text>
 		</g>
 	);
@@ -56,14 +55,14 @@ function CustomTooltip({ active, payload, label }) {
 function CustomLineChart({ data }) {
 	if (data.length > 0) {
 		return (
-			<ResponsiveContainer width="100%" height={300}>
+			<ResponsiveContainer width="90%" height={300}>
 				<LineChart
 					data={data}
 					margin={{
 						top: 0,
-						right: 30,
-						left: 20,
-						bottom: 5,
+						right: 0,
+						left: 50,
+						bottom: 0,
 					}}
 				>
 					<CartesianGrid strokeDasharray="3 3" />
@@ -78,9 +77,99 @@ function CustomLineChart({ data }) {
 	return <span>{lang.you_not_have_information}</span>;
 }
 
+const renderActiveShape = (props) => {
+	const {
+		cx,
+		cy,
+		innerRadius,
+		outerRadius,
+		startAngle,
+		endAngle,
+		payload,
+		percent,
+		value,
+	} = props;
+
+	return (
+		<g>
+			<text
+				x={cx}
+				y={cy - 20}
+				dy={5}
+				fontWeight="bold"
+				textAnchor="middle"
+				fill="var(--blue-light)"
+			>
+				{payload.name}
+			</text>
+			<text
+				x={cx}
+				y={cy + 5}
+				fontWeight="bold"
+				dy={5}
+				textAnchor="middle"
+				fill="var(--green)"
+			>{`R$ ${formatMoney(value)}`}</text>
+			<text
+				x={cx}
+				y={cy + 30}
+				dy={5}
+				textAnchor="middle"
+				fill="var(--blue-light)"
+			>{`${(percent * 100).toFixed(2)}%`}</text>
+			<Sector
+				cx={cx}
+				cy={cy}
+				innerRadius={innerRadius}
+				outerRadius={outerRadius}
+				startAngle={startAngle}
+				endAngle={endAngle}
+				fill="var(--green)"
+			/>
+			<Sector
+				cx={cx}
+				cy={cy}
+				startAngle={startAngle}
+				endAngle={endAngle}
+				innerRadius={outerRadius + 6}
+				outerRadius={outerRadius + 10}
+				fill="var(--green)"
+			/>
+		</g>
+	);
+};
+
+function PieChartCustom({ data }) {
+	const [activeIndex, setIndex] = useState(0);
+	if (data.length > 0) {
+		function onPieEnter(data, index) {
+			setIndex(index);
+		}
+
+		return (
+			<ResponsiveContainer width="100%" height={250}>
+				<PieChart>
+					<Pie
+						activeIndex={activeIndex}
+						activeShape={renderActiveShape}
+						data={data}
+						innerRadius={80}
+						outerRadius={90}
+						fill="var(--blue-light)"
+						dataKey="value"
+						onMouseEnter={onPieEnter}
+					/>
+				</PieChart>
+			</ResponsiveContainer>
+		);
+	}
+	return <span>{lang.you_not_have_information}</span>;
+}
+
 export default function Dashboard(props) {
 	const wallet = useSelector((state) => state.wallet);
 	const price = useSelector((state) => state.price);
+
 	function dataLineChart(name) {
 		let count = 0;
 		const data = [].concat(wallet.operations).reverse();
@@ -128,125 +217,27 @@ export default function Dashboard(props) {
 	const descriptionPie = `${lang.last_updated} ${formatDateFulllHour(
 		new Date()
 	)}`;
+
 	return (
 		<div style={{ minWidth: '550px' }}>
 			<h1 className="title">{lang.dashboard}</h1>
-			<Card title={lang.your_wallet} description={descriptionPie}>
-				<PieChartCustom data={dataPieChart('money')} />
-			</Card>
-
-			<Card title={lang.your_coins} description={descriptionPie}>
-				<PieChartCustom data={dataPieChart('coins')} />
-			</Card>
+			<div className="wrap-card">
+				<Card title={lang.your_wallet} description={descriptionPie}>
+					<PieChartCustom data={dataPieChart('money')} />
+				</Card>
+				<Card title={lang.your_coins} description={descriptionPie}>
+					<PieChartCustom data={dataPieChart('coins')} />
+				</Card>
+			</div>
 			<h1 className="title">{lang.latest_operations}</h1>
-			<Card title="Bitcoin" description={lang.extract}>
-				<CustomLineChart data={dataLineChart('bitcoin')} />
-			</Card>
-
-			<Card title="Brita" description={lang.extract}>
-				<CustomLineChart data={dataLineChart('brita')} />
-			</Card>
+			<div className="wrap-card">
+				<Card title="Bitcoin" description={lang.extract}>
+					<CustomLineChart data={dataLineChart('bitcoin')} />
+				</Card>
+				<Card title="Brita" description={lang.extract}>
+					<CustomLineChart data={dataLineChart('brita')} />
+				</Card>
+			</div>
 		</div>
 	);
-}
-
-const renderActiveShape = (props) => {
-	const RADIAN = Math.PI / 180;
-	const {
-		cx,
-		cy,
-		midAngle,
-		innerRadius,
-		outerRadius,
-		startAngle,
-		endAngle,
-		fill,
-		payload,
-		percent,
-		value,
-	} = props;
-	const sin = Math.sin(-RADIAN * midAngle);
-	const cos = Math.cos(-RADIAN * midAngle);
-	const sx = cx + (outerRadius + 10) * cos;
-	const sy = cy + (outerRadius + 10) * sin;
-	const mx = cx + (outerRadius + 30) * cos;
-	const my = cy + (outerRadius + 30) * sin;
-	const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-	const ey = my;
-	const textAnchor = cos >= 0 ? 'start' : 'end';
-
-	return (
-		<g>
-			<text x={cx} y={cy} dy={8} textAnchor="middle" fill="var(--blue-light)">
-				{payload.name}
-			</text>
-			<Sector
-				cx={cx}
-				cy={cy}
-				innerRadius={innerRadius}
-				outerRadius={outerRadius}
-				startAngle={startAngle}
-				endAngle={endAngle}
-				fill={fill}
-			/>
-			<Sector
-				cx={cx}
-				cy={cy}
-				startAngle={startAngle}
-				endAngle={endAngle}
-				innerRadius={outerRadius + 6}
-				outerRadius={outerRadius + 10}
-				fill={fill}
-			/>
-			<path
-				d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-				stroke={fill}
-				fill="none"
-			/>
-			<circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-			<text
-				x={ex + (cos >= 0 ? 1 : -1) * 12}
-				y={ey}
-				textAnchor={textAnchor}
-				fill="var(--green)"
-			>{`R$ ${formatMoney(value)}`}</text>
-			<text
-				x={ex + (cos >= 0 ? 1 : -1) * 12}
-				y={ey}
-				dy={18}
-				textAnchor={textAnchor}
-				fill="var(--blue-light)"
-			>
-				{`(${(percent * 100).toFixed(2)}%)`}
-			</text>
-		</g>
-	);
-};
-
-function PieChartCustom({ data }) {
-	const [activeIndex, setIndex] = useState(0);
-	if (data.length > 0) {
-		function onPieEnter(data, index) {
-			setIndex(index);
-		}
-
-		return (
-			<ResponsiveContainer width="100%" height={300}>
-				<PieChart>
-					<Pie
-						activeIndex={activeIndex}
-						activeShape={renderActiveShape}
-						data={data}
-						innerRadius={60}
-						outerRadius={80}
-						fill="var(--green)"
-						dataKey="value"
-						onMouseEnter={onPieEnter}
-					/>
-					<Legend verticalAlign="top" height={36} />
-				</PieChart>
-			</ResponsiveContainer>
-		);
-	}
-	return <span>{lang.you_not_have_information}</span>;
 }
